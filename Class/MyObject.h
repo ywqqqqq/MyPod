cc0567701101447cc5a26382326b2581d7c4bdee
颜伟权 <weiquan.yan@jyblife.com>
2021-03-03 18:47:19 +0800

    a

---
+++ Class/MyObject.h
@@ -0,0 +1,17 @@
+//
+//  MyObject.h
+//  AirNetwork
+//
+//  Created by ywq on 2021/3/3.
+//  Copyright © 2021 airone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Class/MyObject.m
@@ -0,0 +1,22 @@
+//
+//  MyObject.m
+//  AirNetwork
+//
+//  Created by ywq on 2021/3/3.
+//  Copyright © 2021 airone. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"a");
+    }
+    return self;
+}
+
+@end
